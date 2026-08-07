0e2f0fb2cc697853829cff835c5be8a45d32de3c
Ashutosh Soni <ashusoni1892@gmail.com>
2026-08-08 04:30:46 +0530

    feat(api): initialize Axum server with health endpoint

--- backend/crates/api/src/main.rs
+++ backend/crates/api/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    routing::get,
+    Json,
+    Router,
+};
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct HealthResponse{
+    service: String,
+    version: String,
+    status: String,
+}
+async fn health()->Json<HealthResponse>{
+    Json(HealthResponse{
+        service: "DexSYS API".into(),
+        version: "0.1.0".into(),
+        status: "running".into(),
+    })
+}
+
+#[tokio::main]
+async fn main(){
+    let app = Router::new().route("/",get(health)).route("/health",get(health));
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8080").await.unwrap();
+    println!("DexSYS API Running at http://127.0.0.1:8080 lessgooo");
+    axum::serve(listener,app).await.unwrap();
 }
